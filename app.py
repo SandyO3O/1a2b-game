a0c9b281deaf8c3f56863e50e3623705f094dead
Cheng Sandy <chengsandy@ChengdeMacBook-Air-2.local>
2025-05-06 22:16:56 +0800

    feat: update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash  # 加入 flash
 import random
 import qrcode
 import os
